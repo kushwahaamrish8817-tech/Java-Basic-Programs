dc361f371c52e9f7cf0f94372a2c60f0fb04e32e
Amrish <kushwahaamrish8817@gmail.com>
2026-06-04 19:15:25 +0530

    Create DataTypesDemo.java

---
+++ 01-Variables-And-DataTypes/DataTypesDemo.java
@@ -0,0 +1,8 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int num = 10;
+        double price = 99.99;
+        char grade = 'A';
+        boolean status = true;
+    }
+}
