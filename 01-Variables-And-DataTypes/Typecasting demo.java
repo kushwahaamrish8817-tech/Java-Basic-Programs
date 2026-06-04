3f8c1ce28a95754398ca270f428dab7205772446
Amrish <kushwahaamrish8817@gmail.com>
2026-06-04 19:15:53 +0530

    Create Typecasting demo.java

---
+++ 01-Variables-And-DataTypes/Typecasting demo.java
@@ -0,0 +1,7 @@
+public class TypeCastingDemo {
+    public static void main(String[] args) {
+        double num = 10.5;
+        int value = (int) num;
+        System.out.println(value);
+    }
+}
