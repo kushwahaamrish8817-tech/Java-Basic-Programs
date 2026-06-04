fb78e0ab16d9d2f5d0fe4848bcb38895a4fe0dd7
Amrish <kushwahaamrish8817@gmail.com>
2026-06-04 19:39:05 +0530

    Create 05-Arrays/ArrayDemo.java

---
+++ 05-Arrays/ArrayDemo.java
@@ -0,0 +1,9 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("First Element: " + numbers[0]);
+        System.out.println("Last Element: " + numbers[4]);
+    }
+}
